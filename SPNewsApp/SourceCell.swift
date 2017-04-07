a1354a1605336f19b61d6c136c0e55f4f654f949
Stepan Paholyk <stepanpaholyk@gmail.com>
2017-04-07 21:03:05 +0300

    Minor changes

---
+++ SPNewsApp/SourceCell.swift
@@ -0,0 +1,29 @@
+//
+//  SourceCell.swift
+//  SPNewsApp
+//
+//  Created by Stepan Paholyk on 4/7/17.
+//  Copyright © 2017 Stepan Paholyk. All rights reserved.
+//
+
+import UIKit
+
+class SourceCell: UITableViewCell {
+
+    @IBOutlet weak var sourceImageView: UIImageView!
+    @IBOutlet weak var sourceLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    
+
+}
