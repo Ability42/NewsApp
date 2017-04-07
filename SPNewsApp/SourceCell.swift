a560da2fcc6e092ca85d0eff1d3446dac94a9a72
Stepan Paholyk <stepanpaholyk@gmail.com>
2017-04-07 23:18:14 +0300

    Add source table view prototype

--- SPNewsApp/SourceCell.swift
+++ SPNewsApp/SourceCell.swift
@@ -12,18 +12,5 @@ class SourceCell: UITableViewCell {
 
     @IBOutlet weak var sourceImageView: UIImageView!
     @IBOutlet weak var sourceLabel: UILabel!
-    
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-    
-    
 
 }
--- SPNewsApp/SourceViewController.swift
+++ SPNewsApp/SourceViewController.swift
@@ -7,41 +7,48 @@
 //
 
 import UIKit
+import SDWebImage
 
 class SourceViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     var sourcesArray = [String]()
-    var sourcesImageArray: UIImage?
-    
+    var sourcesImageArray = [String]()
     let kSourcesGet: String = "https://newsapi.org/v1/sources"
-    
-    
     lazy var manager: ServerManager = ServerManager.init()
     
     @IBOutlet weak var sourceTableView: UITableView!
     
+    override func loadView() {
+        super.loadView()
+        self.fetchSources(withCategory: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.fetchSources(withCategory: nil)
-        self.sourcesArray.removeAll()
-        self.fetchSources(withCategory: "gaming")
-        self.sourcesArray.removeAll()
-        self.fetchSources(withCategory: "business")
-        self.sourcesArray.removeAll()
 
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
+    func setupTableView() {
+
+    }
+    
+    func setupActivityIndicator() {
+        
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return self.sourcesArray.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let reuseID = "sourceCell"
-        let cell = tableView.dequeueReusableCell(withIdentifier: reuseID, for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: reuseID, for: indexPath) as! SourceCell
+        
+        
+        // Cell setup
+        cell.sourceLabel.text = self.sourcesArray[indexPath.item]
+        cell.sourceImageView.sd_setImage(with: URL(string: self.sourcesImageArray[indexPath.item])!)
+        
         return cell
     }
     
@@ -58,29 +65,31 @@ class SourceViewController: UIViewController, UITableViewDelegate, UITableViewDa
         
         self.manager.makeRequest(urlString: urlString, completionHandler: { (data) in
             if !(data?.isEmpty)! {
-                print(urlString)
+                //print(urlString)
                 let jsonResponse = try! JSONSerialization.jsonObject(with: data!, options: []) as! [String : Any]
                 let jsonSources = jsonResponse["sources"] as? [[String : Any]]
                 
                 for source in jsonSources! {
                     let sourceName = source["name"] as! String
-                    self.sourcesArray.append(sourceName)
+                    self.sourcesArray.append(sourceName) // append in array
+                    
+                    let jsonLogos = source["urlsToLogos"] as! [String : String]
+                    let smallImageUrl = jsonLogos["small"]
+                    
+                    self.sourcesImageArray.append(smallImageUrl!)
+//                    print(smallImageUrl!)
+                    print(sourceName)
                 }
                 print(self.sourcesArray.count)
+                print(self.sourcesImageArray.count)
                 
             }
+            DispatchQueue.main.async {
+                self.sourceTableView.reloadData()
+            }
         })
-    }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
     }
-    */
 
 }
+
